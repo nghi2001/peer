6c9fd241cb9196350c0ad01f9009762267df178c
nghi_nguyen <duynghikhum9@gmail.com>
2022-03-29 19:38:29 +0700

    change

--- main.js
+++ main.js
@@ -4,24 +4,6 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server)
 const {v4: uuidV4} = require('uuid');
 
-const { networkInterfaces } = require('os');
-
-const nets = networkInterfaces();
-const results = Object.create(null); // Or just '{}', an empty object
-
-for (const name of Object.keys(nets)) {
-    for (const net of nets[name]) {
-        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-        if (net.family === 'IPv4' && !net.internal) {
-            if (!results[name]) {
-                results[name] = [];
-            }
-            results[name].push(net.address);
-        }
-    }
-    
-console.log(Object.values(results)[0][0]);
-}
 app.set('view engine','ejs');
 app.use(express.static('public'))
 
